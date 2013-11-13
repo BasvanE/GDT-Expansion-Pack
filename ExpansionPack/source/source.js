c9a1aa772be4ce5828e92b94716a378c1ff2f849
LineLiar <gamelover34-gamelover34@hotmail.com>
2013-11-13 20:14:13 +0100

    Update 0.2.2

--- ExpansionPack/source/source.js
+++ ExpansionPack/source/source.js
@@ -426,6 +426,41 @@ var ExpPack = {};
 			});
 	};
 	/*  */
+
+	/* Itara */
+	ExpPack.addPlatformItaraBackflash = function () {
+		var icon = './mods/ExpansionPack/source/img/Itara.png';
+		GDT.addPlatform(
+			{
+				id: '31102000-2-1-4-1-LINELIAR',
+				name: 'Itara Backflash',
+				company: 'Itara',
+				startAmount: 1.3,
+				unitsSold: 4.2,
+				licencePrize: 150000,
+				published: '12/11/3',
+				platformRetireDate: '16/1/2',
+				developmentCosts: 125000,
+				genreWeightings: [0.8, 1, 0.6, 0.7, 1, 0.6],
+				audienceWeightings: [0.8, 1, 0.7],
+				techLevel: 2,
+				iconUri: icon,
+				events: [
+					{
+						id: '31102000-2-1-4-1-1-LINELIAR',
+						date: '12/7/3',
+						getNotification: function (company) {
+							return new Notification({
+								header: "Industry News".localize(),
+								text: "Today Itara, a company known for their consoles in the early consolemarket, have announced that they're gonna release a new console called the Itara Backflash.{n} This console will compete with consoles like the TES. Itara has announced that they are anticipating the console to be very successfull.{n} The Itara Flashback is getting released in {0}.".localize().format(General.getETADescription('12/7/3', '12/11/3')),
+								image: icon
+							});
+						}
+					}
+				]
+			});
+	};
+	/*  */
 	
 	/* Ninvento */	
 	ExpPack.addPlatform3GS = function () {			
@@ -442,7 +477,7 @@ var ExpPack = {};
 				platformRetireDate: '24/5/1',
 				developmentCosts: 80000,
 				genreWeightings: [0.7, 1, 0.8, 1, 0.6, 1],
-				audienceWeightings: [1, 0.8, 0.6],
+				audienceWeightings: [0.9, 1, 0.6],
 				techLevel: 4,
 				iconUri: icon,
 				events: [
