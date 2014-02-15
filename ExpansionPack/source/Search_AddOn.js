344fda1d97b177d0b2c8e1c5355f8e23056ca60c
Bas v. Elzakker <basvanelzakker@hotmail.com>
2014-02-15 23:19:34 +0100

    Lower case finding
    Entering lower case topics works now as well, with the help of Alphabit
    :D

--- ExpansionPack/source/Search_AddOn.js
+++ ExpansionPack/source/Search_AddOn.js
@@ -9,20 +9,25 @@ var ExpPack = {};
 		var search = $('<span style="font-size:18px; margin-left: 40px;">Search Topic: </span><input id="filterbox" type="text" value="" maxlength="35" style="font-size: 22pt; width: 200px;" />')
 		titel.append(search);
 		
+		jQuery.expr[':'].epmcontains = function(a, i, m) {
+			return jQuery(a).text().toUpperCase()
+				.indexOf(m[3].toUpperCase()) >= 0;
+		};
+		
 		$('#filterbox').on('input', function(e){
 			var searchstr = $(this).val();
-	
+			
 			$('#myoutput').text(searchstr);
-	
+			
 			$('.listContainer > *').show();
-	    
+	    	
 			if(searchstr === "" || searchstr === "*"){
 				$('.listContainer > *');
 				return;
 			}
 			if(searchstr.length > 0){
-				$('.listContainer > :not(:contains('+searchstr+'))').hide();
-				$('.listContainer > :contains('+searchstr+')');
+				$('.listContainer > :not(:epmcontains('+searchstr+'))').hide();
+				$('.listContainer > :epmcontains('+searchstr+')');
 			}
 		});
 	}
